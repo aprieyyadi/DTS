66b9410fdd1809197e42be67228f563098d70bee
danielchandra2 <56572103+danielchandra2@users.noreply.github.com>
2019-10-20 22:38:14 +0700

    Bersih Bersih

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -112,14 +112,7 @@
           <!-- END main-content -->
 
           <div class="col-md-12 col-lg-4 sidebar">
-            <div class="sidebar-box search-form-wrap">
-              <form action="#" class="search-form">
-                <div class="form-group">
-                  <span class="icon fa fa-search"></span>
-                  <input type="text" class="form-control" id="s" placeholder="Type a keyword and hit enter">
-                </div>
-              </form>
-            </div>
+            
             <!-- END sidebar-box -->
             <div class="sidebar-box">
               <div class="bio text-center">
@@ -128,12 +121,6 @@
                   <h2>{{$post->user->name}}</h2>
                   <p></p>
                   <p><a href="{{ route('author.profile',$post->user->username) }}" class="btn btn-primary btn-sm rounded">Read my bio</a></p>
-                  <p class="social">
-                    <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
-                    <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
-                    <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
-                    <a href="#" class="p-2"><span class="fa fa-youtube-play"></span></a>
-                  </p>
                 </div>
               </div>
             </div>
